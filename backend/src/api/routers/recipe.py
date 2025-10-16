04edd84bbba3ffdc1b4eb35c506e30094fa21efe
paulv <paulvorderbruegge@proton.me>
2025-10-16 18:16:23 +0700

    added comments in routers/recipe.py

--- backend/src/api/routers/recipe.py
+++ backend/src/api/routers/recipe.py
@@ -1,3 +1,8 @@
 from ...services.agent_service import AgentService
 
 agent_service = AgentService()
+
+'''
+POST generate_recipe, change_recipe, save_recipe, start_recipe, change_state, ask_question, finish_recipe
+GET view_recipe, view_options, get_recipe, get_state, get_prompt_history
+'''
