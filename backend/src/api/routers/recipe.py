9775678cae6ecb09a06ab002eb92951501ab042c
paulv <paulvorderbruegge@proton.me>
2025-10-16 21:25:51 +0700

    added api/schemas/recipe.py

--- backend/src/api/routers/recipe.py
+++ backend/src/api/routers/recipe.py
@@ -3,6 +3,6 @@ from ...services.agent_service import AgentService
 agent_service = AgentService()
 
 '''
-POST generate_recipe, change_recipe_ai, change_recipe_manual, save_recipe, start_recipe, change_state, ask_question, finish_recipe
+POST generate_recipe, change_recipe_ai, change_recipe_manual, save_recipe, start_recipe, change_state, ask_question
 GET view_options, get_recipe, get_state, get_prompt_history
 '''
