aa0284f99fcab0f1344b2b76f06567fe902393c1
lucabzt <luca.bozzetti8@gmail.com>
2025-10-16 19:28:47 +0800

    kp

--- backend/src/api/routers/recipe.py
+++ backend/src/api/routers/recipe.py
@@ -3,6 +3,6 @@ from ...services.agent_service import AgentService
 agent_service = AgentService()
 
 '''
-POST generate_recipe, change_recipe, save_recipe, start_recipe, change_state, ask_question, finish_recipe
-GET view_recipe, view_options, get_recipe, get_state, get_prompt_history
+POST generate_recipe, change_recipe_ai, change_recipe_manual, save_recipe, start_recipe, change_state, ask_question, finish_recipe
+GET view_options, get_recipe, get_state, get_prompt_history
 '''
